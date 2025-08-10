d5debcf09d270a51f705679b5970a1c0d590ed26
kishikishore2863 <kishikishore.2863@gmail.com>
2025-08-10 21:39:26 +0530

    bitwise operation

---
+++ IntroductionToC/src/dataTypes_operators/p5.c
@@ -0,0 +1,10 @@
+//
+// Created by Kishi Kishore N on 10/08/25.
+//
+#include <stdio.h>
+
+void main() {
+    int a=7,b=21,c;
+    c= a&b;
+    printf("%d",c);
+}
---
+++ IntroductionToC/src/dataTypes_operators/p6.c
@@ -0,0 +1,9 @@
+//
+// Created by Kishi Kishore N on 10/08/25.
+//
+#include <stdio.h>
+void main() {
+    int a=5,b=17,c;
+    c= a&b;
+    printf("%d",c);
+}
